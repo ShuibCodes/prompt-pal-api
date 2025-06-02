ead6d2d92f0bf1b8ccd5f57705e2092abffd7e3b
RegiLoshi <rloshi111@gmail.com>
2025-06-02 11:47:39 +0200

    Fixed backend url

--- config/security.ts
+++ config/security.ts
@@ -1,7 +1,11 @@
 export default ({ env }) => ({
   cors: {
     enabled: true,
-    origin: ['https://prompt-pal-front-end.vercel.app'],
+    origin: [
+      'https://prompt-pal-front-end.vercel.app',
+      'http://localhost:5173',
+      env('FRONTEND_URL', 'http://localhost:5173')
+    ],
     headers: ['*'],
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'],
     keepHeaderOnError: true,
--- config/server.ts
+++ config/server.ts
@@ -1,7 +1,7 @@
 export default ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  url: env('FRONTEND_URL', 'http://localhost:5173'),
+  url: env('BACKEND_URL', 'https://prompt-pal-api.onrender.com'),
   app: {
     keys: env.array('APP_KEYS'),
   },
