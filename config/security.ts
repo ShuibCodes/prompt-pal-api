7f93f93eac0f2645a55e198f65799a07e8223a97
RegiLoshi <rloshi111@gmail.com>
2025-06-18 19:39:25 +0200

    Cors update

--- config/security.ts
+++ config/security.ts
@@ -7,6 +7,10 @@ export default ({ env }) => ({
       'http://localhost:5174',
       'https://prompt-analyzer-client.vercel.app',
       'https://prompt-pal-front-end.vercel.app',
+      'https://edu-sync-5s2x-git-feat-prompt-pal-i-1098a1-shuaybcodes-projects.vercel.app',
+      'edu-sync-5s2x-git-feat-prompt-pal-i-1098a1-shuaybcodes-projects.vercel.app',
+      'edu-sync-5s2x-o0oi0mnj8-shuaybcodes-projects.vercel.app',
+      'edu-sync-5s2x-git-testing-shuaybcodes-projects.vercel.app',
       env('FRONTEND_URL', 'http://localhost:5173')
     ],
     headers: ['*'],
